0774dca89e04b79e786c0f47b60bf8551d6bf947
Henry Chung <35611465+hchung11@users.noreply.github.com>
2020-05-17 17:13:46 -0400

    first commit

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Jerry Zacharias on 5/17/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
